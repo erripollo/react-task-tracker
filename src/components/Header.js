b9f45138c3c501fec6ce1da62fc3f83f730c4e17
Enrico <78877890+erripollo@users.noreply.github.com>
2021-09-21 23:15:15 +0200

    Create form add task

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,15 +2,16 @@ import PropTypes from 'prop-types'
 import Button from './Button'
 
 //passo come parametro della funzione una props 'title'
-const Header = ({ title }) => {
-    const onClick = () => {
-        console.log('Click');
-    }
+const Header = ({ title, onAdd, showAdd }) => {
 
     return (
         <header className='header'>
             <h1>{title}</h1>
-            <Button color='green' text='Add' onClick={onClick} />
+            <Button 
+                color={showAdd ? 'red' : 'green'} 
+                text={showAdd ? 'Close' : 'Add'} 
+                onClick={onAdd} 
+            />
         </header>
     )
 }
