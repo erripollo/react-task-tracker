3073dc69bc41038f25e17cf6f93b744afee93654
Enrico <78877890+erripollo@users.noreply.github.com>
2021-09-21 12:49:22 +0200

    Create Button component and add to Header component

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import PropTypes from 'prop-types'
+import Button from './Button'
 
 //passo come parametro della funzione una props 'title'
 const Header = ({ title }) => {
+    const onClick = () => {
+        console.log('Click');
+    }
+
     return (
-        <header>
+        <header className='header'>
             <h1>{title}</h1>
+            <Button color='green' text='Add' onClick={onClick} />
         </header>
     )
 }
@@ -18,5 +24,12 @@ Header.defaultProps = {
 Header.propTypes = {
     title: PropTypes.string.isRequired,
 }
+
+
+//esempio di CSS in JS... aggingere nell'elemtno es.<h1></h1> lo stile inline style={headingStyle}
+/* const headingStyle = {
+    color: 'red',
+    backgroundColor: 'black',
+} */
 
 export default Header
